bbc231b419c98e94596ccd12f334fef14b7ddfc5
ECHONESSY\Echonessy <545625899@qq.com>
2018-12-28 10:31:13 +0800

    新增fs日志

--- controllers/key.js
+++ controllers/key.js
@@ -20,6 +20,9 @@ exports.writeKeyFile = function (req, res) {
         resData.reqCode = '0000';
         resData.msg = '写入成功';
         if (err) {
+            console.log('---------------------err------------------------')
+            console.log(err);
+            console.log('---------------------err------------------------')
             resData.result = 'fail';
             resData.reqCode = '9999';
             resData.msg = '写入失败,请重试';
@@ -40,6 +43,9 @@ exports.readKeyFile = function (req, res) {
         resData.msg = '读取成功';
         resData.data = data;
         if (err) {
+            console.log('---------------------err------------------------')
+            console.log(err);
+            console.log('---------------------err------------------------')
             resData.result = 'fail';
             resData.reqCode = '9999';
             resData.msg = '文件读取异常';
